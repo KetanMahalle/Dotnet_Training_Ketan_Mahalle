af177488397e39e8161ee416d5adf324dd71f242
KetanMahalle <ketanmahalle777@gmail.com>
2024-05-21 13:24:25 +0530

    updated code

--- Ketan_Mahalle_Assignment2/Program.cs
+++ Ketan_Mahalle_Assignment2/Program.cs
@@ -1,4 +1,111 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Item
+{
+    public int ID;
+    public string Name;
+    public double Price;
+    public int Quantity;
+
+    public Item(int id, string name, double price, int quantity)
+    {
+        ID = id;
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+        string result = "ID:"+ID+" ||  Name:"+Name+ " ||  Price:"+Price+"  || Quantity: "+Quantity;
+        return result;
+    }
+}
+
+public class Inventory
+{
+    public List<Item> items;
+
+    public Inventory()
+    {
+        items = new List<Item>();
+    }
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+        Console.WriteLine("Item added successfully.");
+    }
+
+    public void DisplayAllItems()
+    {
+        if (items.Count == 0)
+        {
+            Console.WriteLine("No items in inventory.");
+            return;
+        }
+
+        //Converting List in array for assignment.
+        //Though we can simply iterate over list as well.
+        /*   foreach (var item in items)
+             {
+                 Console.WriteLine(item);
+             }                                   
+        */
+
+        Item[] itemsArray = items.ToArray();
+        foreach (var item in itemsArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public Item FindItemById(int id)
+    {
+        foreach (var item in items)
+        {
+            if (item.ID == id)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
+
+    public void UpdateItem(int id, string newName, double newPrice, int newQuantity)
+    {
+        var item = FindItemById(id);
+        if (item != null)
+        {
+            item.Name = newName;
+            item.Price = newPrice;
+            item.Quantity = newQuantity;
+            Console.WriteLine("Item updated successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Item not found.");
+        }
+    }
+
+    public void DeleteItem(int id)
+    {
+        var item = FindItemById(id);
+        if (item != null)
+        {
+            items.Remove(item);
+            Console.WriteLine("Item deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Item not exist. Plz ensure correct id to Delete.");
+        }
+    }
+}
+
+
 
 class Program
 {
